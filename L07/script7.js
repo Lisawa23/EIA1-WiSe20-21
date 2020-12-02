ba5f982c2c88a254d2af6baf89d75a3faadfe0dd
Lisa <lisa.maria.waletzko@hs-furtwangen.de>
2020-12-02 16:00:39 +0100

    L07 hochgeladen

---
+++ L07/script7.js
@@ -0,0 +1,48 @@
+var sound1 = new Audio('../assets/A.mp3');
+var sound2 = new Audio('../assets/C.mp3');
+var sound3 = new Audio('../assets/F.mp3');
+var sound4 = new Audio('../assets/G.mp3');
+var sound5 = new Audio('../assets/hihat.mp3');
+var sound6 = new Audio('../assets/kick.mp3');
+var sound7 = new Audio('../assets/laugh-1.mp3');
+var sound8 = new Audio('../assets/laugh-2.mp3');
+var sound9 = new Audio('../assets/drums.mp3');
+//button
+//playSample = Funktion
+//Der Beat bzw. die Reihenfolge der abzuspielenden Samples sollte in einem Array gespeichert werden.
+function playSample(sound) {
+    document.getElementById('#square').innerHTML = sound;
+}
+//EventListener
+document.querySelector(".square1").addEventListener('click', function () {
+    sound1.play();
+});
+document.querySelector(".square2").addEventListener('click', function () {
+    sound2.play();
+});
+document.querySelector(".square3").addEventListener('click', function () {
+    sound3.play();
+});
+document.querySelector(".square4").addEventListener('click', function () {
+    sound4.play();
+});
+document.querySelector(".square5").addEventListener('click', function () {
+    sound5.play();
+});
+document.querySelector(".square6").addEventListener('click', function () {
+    sound6.play();
+});
+document.querySelector(".square7").addEventListener('click', function () {
+    sound7.play();
+});
+document.querySelector(".square8").addEventListener('click', function () {
+    sound8.play();
+});
+document.querySelector(".square9").addEventListener('click', function () {
+    sound9.play();
+});
+//Intervall
+setInterval(function () {
+    //Anweisung
+}, 500);
+//# sourceMappingURL=script7.js.map
---
+++ L07/script7.ts
@@ -0,0 +1,60 @@
+const sound1:HTMLAudioElement = new Audio('../assets/A.mp3');
+const sound2:HTMLAudioElement = new Audio('../assets/C.mp3');
+const sound3:HTMLAudioElement = new Audio('../assets/F.mp3');
+const sound4:HTMLAudioElement = new Audio('../assets/G.mp3');
+const sound5:HTMLAudioElement = new Audio('../assets/hihat.mp3');
+const sound6:HTMLAudioElement = new Audio('../assets/kick.mp3');
+const sound7:HTMLAudioElement = new Audio('../assets/laugh-1.mp3');
+const sound8:HTMLAudioElement = new Audio('../assets/laugh-2.mp3');
+const sound9:HTMLAudioElement = new Audio('../assets/drums.mp3');
+
+//button
+//playSample = Funktion
+//Der Beat bzw. die Reihenfolge der abzuspielenden Samples sollte in einem Array gespeichert werden.
+function playSample(sound:string): any{
+    document.getElementById('#square').innerHTML = sound;
+}
+
+//EventListener
+document.querySelector(".square1").addEventListener('click', function(){
+    sound1.play();
+    
+})
+
+document.querySelector(".square2").addEventListener('click', function(){
+    sound2.play();
+})
+
+document.querySelector(".square3").addEventListener('click', function(){
+    sound3.play();
+})
+
+document.querySelector(".square4").addEventListener('click', function(){
+    sound4.play();
+})
+
+document.querySelector(".square5").addEventListener('click', function(){
+    sound5.play();
+})
+
+document.querySelector(".square6").addEventListener('click', function(){
+    sound6.play();
+})
+
+document.querySelector(".square7").addEventListener('click', function(){
+    sound7.play();
+})
+
+document.querySelector(".square8").addEventListener('click', function(){
+    sound8.play();
+})
+
+document.querySelector(".square9").addEventListener('click', function(){
+    sound9.play();
+})
+
+
+//Intervall
+setInterval(function() {
+    //Anweisung
+   }, 500);
