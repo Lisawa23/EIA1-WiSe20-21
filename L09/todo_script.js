a41143776c9f7a73af5efd2eb6709f09396b6124
Lisa <lisa.maria.waletzko@hs-furtwangen.de>
2020-12-16 23:33:58 +0100

    endlich funktioniert alles

--- L09/todo_script.js
+++ L09/todo_script.js
@@ -1,5 +1,5 @@
 //LoadButton
-document.querySelector(".fa-sync-alt").addEventListener("click", function () {
+document.querySelector("#clear").addEventListener("click", function () {
     localStorage.clear(); //Lokaler Speicher
     location.reload();
 });
@@ -8,7 +8,6 @@ loadEvents();
 // load every event in the page
 function loadEvents() {
     document.querySelector("form").addEventListener("submit", submit);
-    document.querySelector("#clear").addEventListener("click", clearList);
 }
 function submit(e) {
     e.preventDefault();
@@ -18,71 +17,28 @@ function submit(e) {
     input.value = "";
 }
 // add tasks
+var countminus = document.getElementsByClassName("deletetodo");
+var countplus = document.getElementsByClassName("newtodo");
 function addList(task) {
     var ul = document.querySelector("ul");
     var li = document.createElement("li");
-    li.innerHTML = "<i class=\"far delete fa-trash-alt\"></i><input type=\"checkbox\"><label class=\"nameElement\">" + task + "</label>";
+    li.innerHTML = "<span class=\"deletetodo\"><i class=\"far delete fa-trash-alt\"><span/></i><input class=\"newtodo\" type=\"checkbox\"><label class=\"nameElement\">" + task + "</label>";
     ul.appendChild(li);
-    li.querySelector(".fa-trash-alt").addEventListener("click", deleteElement);
-    // document.querySelector("form").addEventListener("submit", coNumber);
+    li.querySelector(".deletetodo").addEventListener("click", deleteElement);
+    for (var i = 0; i < countplus.length; i++) {
+        countplus[i].addEventListener("submit", submit);
+    }
+    for (var i = 0; i < countplus.length; i++) {
+        countminus[i].addEventListener("click", deleteElement);
+    }
+    listnumber();
 }
-//Clear List
-function clearList(e) {
-    e.preventDefault();
-    var ul = document.querySelector("ul");
-    ul.innerHTML = "";
+function listnumber() {
+    document.querySelector(".number").innerHTML = countplus.length + " in total";
 }
 // Delete ListElement
-function deleteElement(e) {
-    e.preventDefault();
-    var ul = document.querySelector("ul");
-    var li = document.querySelector("li");
-    ul.removeChild(li);
-    // document.querySelector("form").addEventListener("submit", delNumber);
+function deleteElement() {
+    this.parentElement.remove();
+    listnumber();
 }
-//Number in total
-// var newNumber: number = 30;  
-// var index: number = 0;
-// function coNumber(e: Event): void {
-//   e.preventDefault();
-//   while (index < newNumber) {
-//       document.querySelector(".number").innerHTML = + index + " in total"; 
-//       index ++; }
-// }
-// function delNumber(e: Event): void {
-//   e.preventDefault();
-//   while (index < newNumber) {
-//     document.querySelector(".number").innerHTML = + index + " in total"; 
-//     index --; }
-// }
-// var newNumber: number = 30;  
-// var index: number = 0;
-// function coNumber(): void {
-// for (var i: number = 0; i <= newNumber; i ++) { 
-//   if (i == 0) { 
-//     document.querySelector(".number").innerHTML = + i + " in total";
-//   }
-//   i ++;
-// } 
-// else {
-//   return;
-// }
-// }
-// function updateListCount(): void {
-//   var count: HTMLElement = $("li").length;
-//   $("h3").text(+ count + " in total");
-// }
-// ->  updateListcOUNT(); bei add und remove hinschreiben
-// var newNumber: number = 30;
-// var index: number = 0;
-// var newNumber: number = 30;
-// function coNumber (): void {
-//   for (let index: number = 0; index < newNumber; index++) { 
-//     document.querySelector(".number").innerHTML = + index + " in total"; }
-// }
-// function delNumber(): void {
-//   for (index = 0; index < newNumber; index++); {
-//     document.querySelector(".number").innerHTML = + newNumber + " in total"; 
-//     index --; }
-// }
 //# sourceMappingURL=todo_script.js.map
--- L09/todo_script.ts
+++ L09/todo_script.ts
@@ -1,5 +1,5 @@
 //LoadButton
-document.querySelector(".fa-sync-alt").addEventListener("click", function(): void {
+document.querySelector("#clear").addEventListener("click", function(): void {
     localStorage.clear(); //Lokaler Speicher
     location.reload();
 });
@@ -9,7 +9,6 @@ loadEvents();
 // load every event in the page
 function loadEvents(): void {
   document.querySelector("form").addEventListener("submit", submit);
-  document.querySelector("#clear").addEventListener("click", clearList);
 }
 
 function submit(e: Event): void {
@@ -19,91 +18,33 @@ function submit(e: Event): void {
     addList(input.value);
   input.value = "";
 }
-  
+
 // add tasks
+const countminus: HTMLCollection = document.getElementsByClassName("deletetodo");
+const countplus: HTMLCollection = document.getElementsByClassName("newtodo");
+
 function addList(task: string): void {
   let ul: HTMLUListElement = document.querySelector("ul");
   let li: HTMLLIElement = document.createElement("li");
-  li.innerHTML = `<i class="far delete fa-trash-alt"></i><input type="checkbox"><label class="nameElement">${task}</label>`;
+  li.innerHTML = `<span class="deletetodo"><i class="far delete fa-trash-alt"><span/></i><input class="newtodo" type="checkbox"><label class="nameElement">${task}</label>`;
   ul.appendChild(li);
-  li.querySelector(".fa-trash-alt").addEventListener("click", deleteElement);
-  // document.querySelector("form").addEventListener("submit", coNumber);
+  li.querySelector(".deletetodo").addEventListener("click", deleteElement);
+
+  for (let i: number = 0; i < countplus.length; i ++) { 
+    countplus[i].addEventListener("submit", submit);
+  }
+  for (let i: number = 0; i < countplus.length; i ++) { 
+    countminus[i].addEventListener("click", deleteElement);
+  }
+  listnumber();
 }
 
-//Clear List
-function clearList(e: Event): void {
-  e.preventDefault();
-  let ul: HTMLUListElement = document.querySelector("ul");
-  ul.innerHTML = "";
+function listnumber(): void {
+  document.querySelector(".number").innerHTML = countplus.length + " in total"; 
 }
 
 // Delete ListElement
-function deleteElement(e: Event): void {
-  e.preventDefault();
-  let ul: HTMLUListElement = document.querySelector("ul");
-  let li: HTMLLIElement = document.querySelector("li"); 
-  ul.removeChild(li);
-  // document.querySelector("form").addEventListener("submit", delNumber);
+function deleteElement(): void {
+  this.parentElement.remove(); 
+  listnumber();
 }
-
-
-//Number in total
-// var newNumber: number = 30;  
-// var index: number = 0;
-
-// function coNumber(e: Event): void {
-//   e.preventDefault();
-//   while (index < newNumber) {
-//       document.querySelector(".number").innerHTML = + index + " in total"; 
-//       index ++; }
-// }
-
-// function delNumber(e: Event): void {
-//   e.preventDefault();
-//   while (index < newNumber) {
-//     document.querySelector(".number").innerHTML = + index + " in total"; 
-//     index --; }
-// }
-
-// var newNumber: number = 30;  
-// var index: number = 0;
-
-// function coNumber(): void {
-// for (var i: number = 0; i <= newNumber; i ++) { 
-//   if (i == 0) { 
-
-//     document.querySelector(".number").innerHTML = + i + " in total";
-//   }
-//   i ++;
-// } 
-// else {
-//   return;
-// }
-// }
-
-
-
-
-
-// function updateListCount(): void {
-//   var count: HTMLElement = $("li").length;
-//   $("h3").text(+ count + " in total");
-// }
-// ->  updateListcOUNT(); bei add und remove hinschreiben
-
-
-
-
-// var newNumber: number = 30;
-// var index: number = 0;
-// var newNumber: number = 30;
-// function coNumber (): void {
-//   for (let index: number = 0; index < newNumber; index++) { 
-//     document.querySelector(".number").innerHTML = + index + " in total"; }
-// }
-
-// function delNumber(): void {
-//   for (index = 0; index < newNumber; index++); {
-//     document.querySelector(".number").innerHTML = + newNumber + " in total"; 
-//     index --; }
-// }
