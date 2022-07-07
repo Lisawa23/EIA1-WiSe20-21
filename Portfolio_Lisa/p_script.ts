ac82145f847876fd7236b49d4bb703b098276434
Lisawa23 <lisa.maria.waletzko@hs-furtwangen.de>
2022-07-07 19:24:31 +0200

    update Poprtfolio

--- Portfolio_Lisa/p_script.ts
+++ Portfolio_Lisa/p_script.ts
@@ -13,26 +13,26 @@ window.addEventListener("load", function(): void {
       }
     });
     var spr: string [] = [];
-    spr[0]="CREATIVE    ";
-    spr[1]="PUNCTUAL    ";
-    spr[2]="INDIVIDUAL    ";
-    spr[3]="HARDWORKING    ";
-    spr[4]="RELIABLY    ";
-    spr[5]="ADVENTUROUS    ";
-    spr[6]="AMBITIOUS    ";
-    spr[7]="HUMOROUS    ";
-    spr[8]="COMMUNICATIVE    ";
+    spr[0] = "CREATIVE    ";
+    spr[1] = "PUNCTUAL    ";
+    spr[2] = "INDIVIDUAL    ";
+    spr[3] = "HARDWORKING    ";
+    spr[4] = "RELIABLY    ";
+    spr[5] = "ADVENTUROUS    ";
+    spr[6] = "AMBITIOUS    ";
+    spr[7] = "HUMOROUS    ";
+    spr[8] = "COMMUNICATIVE    ";
     let n: number = 0;
     let m: number = 0;
-    function tick(): void{
+    function tick(): void {
        n++;
-        if(n>spr[m].length){
-          n=1;
-          m++
+       if (n > spr[m].length) {
+          n = 1;
+          m++;
          }
-       if(m>8)m=0;
-       document.querySelector("#typed").innerHTML = spr[m].slice(0,n);
-       window.setTimeout(tick,300);
+       if (m > 8)m = 0;
+       document.querySelector("#typed").innerHTML = spr[m].slice(0, n);
+       window.setTimeout(tick, 300);
     }
-    window.setTimeout(tick,500);
-})
+    window.setTimeout(tick, 500);
+});
